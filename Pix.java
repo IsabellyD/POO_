1f34e38487054323fc9d5c005c2a518ab4989749
Isabelly_Dias <145844761+IsabellyD@users.noreply.github.com>
2025-11-16 19:20:04 -0300

    Add Pix class for payment processing

---
+++ Pix.java
@@ -0,0 +1,21 @@
+public class Pix extends Pagamento {
+    private String chave;
+
+    public Pix(String nomeCliente, double valor, String chave) {
+        super(nomeCliente, valor);
+        this.chave = chave;
+    }
+
+    @Override
+    public double processarPagamento() {
+        return getValor(); // sem taxa
+    }
+
+    @Override
+    public String toString() {
+        return "PIX | Cliente: " + getNomeCliente() +
+               " | Chave: " + chave +
+               " | Valor base: R$ " + getValor() +
+               " | Final: R$ " + processarPagamento();
+    }
+}
